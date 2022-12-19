248e7bb0d0549158c7a4b68bd23977c5470bb8d0
befragment <109626726+befragment@users.noreply.github.com>
2022-12-20 00:27:22 +0300

    Create 1537B.cpp

---
+++ 1537B.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+ 
+int main() {
+    int t = 0;
+    std::cin >> t;
+    while(t--) {
+        int a = 0, b = 0, c = 0, d = 0;
+        std::cin >> a >> b >> c >> d;
+        std::cout << "1 1 " << a << ' ' << b << std::endl;
+    }
+    return 0;
+}
