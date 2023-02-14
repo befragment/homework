b32fa13334ecb33f9a2413931e0070c3150eebd8
befragment <109626726+befragment@users.noreply.github.com>
2023-02-14 22:34:54 +0300

    Update Rational.cpp

--- rationalnumbers/RationalLibrary/Rational.cpp
+++ rationalnumbers/RationalLibrary/Rational.cpp
@@ -25,3 +25,42 @@ std::istream& operator>>(std::istream& istream, Rational& frac) {
     }
     return istream;
 }
+
+
+Rational abs(Rational& frac) {
+    frac.num = std::abs(frac.num);
+    frac.denum = std::abs(frac.denum);
+    return {frac.num, frac.denum};
+}
+
+Rational reversed_fraction(Rational& arg) {
+    Rational new_arg(1, 1);
+    if (arg.num > 0) {
+        new_arg.num = arg.denum;
+        new_arg.denum = arg.num;
+    } else if (arg.num == 0) {
+        new_arg = {0, 1};
+    } else {
+        new_arg.num = -arg.denum;
+        new_arg.denum = std::abs(arg.num);
+    }
+    return new_arg;
+}
+
+Rational pow(Rational& frac, int32_t& exp) {
+    Rational new_frac(1, 1);
+    if (frac.num == 0 && exp == 0) {
+        throw std::invalid_argument("mathematical uncertainty: 0^0");
+    }
+    if (exp >= 0) {
+        for (int i = 0; i < exp; i++) {
+            new_frac *= frac;
+        }
+    } else {
+        frac = reversed_fraction(frac);
+        for (int i = 0; i < std::abs(exp); i++) {
+            new_frac *= frac;
+        }
+    }
+    return new_frac.frac_reduction();
+}
